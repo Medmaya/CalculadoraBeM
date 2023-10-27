83132f68e6be8355442b4e59860a6cb70d1a67a6
Medmaya <medmay2019@gmail.com>
2023-10-26 22:18:55 -0300

    first commit

---
+++ js/javinha.js
@@ -0,0 +1,16 @@
+function result(n) {
+
+        var r1 = document.getElementById("cx").value;
+
+        if (r1 <= 5) 
+            alert("O valor inserido é muito baixo, sua namorada vai chorar se ficar sabendo disso. Favor inserir um valor decente.");
+       else if (r1 <= 10) 
+            alert("Sua namorada definitivamente te ama mais.");
+       else if (r1 <= 12) 
+            alert("Até que você ama bastantinho, mas sua namorada com certeza ainda ama mais.");
+        else 
+            alert("Boa tentativa, mas o amor da sua namorada por você, cujo valor beira o infinito, ainda é maior.");
+}
+
+
+
